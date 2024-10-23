8af50f9888c6df7730b63a5ff106d60a9bd1d8cf
Alexandr_Tikhonov <tihas75@mail.ru>
2024-10-23 17:33:01 +0300

    add up to and including les_11

---
+++ les_10_"Спецсимволы, экранирование символов, raw-строки".py
@@ -0,0 +1,19 @@
+# text = "Hello\nworld"
+#
+# print(text)
+# print(len(text))
+from pprint import pprint
+
+# #----------------------------------------
+# text_1 = '\tpanda needs\npython'
+# print(text_1)
+
+#----------------------------------------
+# экранирование \
+
+# path = "D:\\Python\\Project\\stepik\\text_1.py"
+path_1 = r"D:\Python\Project\stepik\text_1.py"
+# print(path)
+
+sort_ = "Марка вина \"Ягода\""
+pprint(sort_)
